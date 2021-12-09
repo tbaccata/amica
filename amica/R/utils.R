0d4efed0a8545adb5ff4b1c725b8af09f21dedcc
tbaccata <sebastian.didusch@gmail.com>
2021-12-09 16:58:26 +0100

    added pairwise overlap calculation

--- amica/R/utils.R
+++ amica/R/utils.R
@@ -1355,3 +1355,74 @@ dfs2gml <- function(nodes, edges) {
   return(outstring)
 }
 
+
+### ID PROTEIN OVERLAP
+# https://blog.jdblischak.com/posts/pairwise-overlaps/
+calc_pairwise_overlaps <- function(sets) {
+  # Ensure that all sets are unique character vectors
+  sets_are_vectors <- vapply(sets, is.vector, logical(1))
+  if (any(!sets_are_vectors)) {
+    stop("Sets must be vectors")
+  }
+  sets_are_atomic <- vapply(sets, is.atomic, logical(1))
+  if (any(!sets_are_atomic)) {
+    stop("Sets must be atomic vectors, i.e. not lists")
+  }
+  sets <- lapply(sets, as.character)
+  is_unique <- function(x) length(unique(x)) == length(x)
+  sets_are_unique <- vapply(sets, is_unique, logical(1))
+  if (any(!sets_are_unique)) {
+    stop("Sets must be unique, i.e. no duplicated elements")
+  }
+  
+  n_sets <- length(sets)
+  set_names <- names(sets)
+  n_overlaps <- choose(n = n_sets, k = 2)
+  
+  vec_name1 <- character(length = n_overlaps)
+  vec_name2 <- character(length = n_overlaps)
+  vec_num_sample1 <- integer(length = n_overlaps)
+  vec_num_sample2 <- integer(length = n_overlaps)
+  vec_num_shared <- integer(length = n_overlaps)
+  vec_overlap <- numeric(length = n_overlaps)
+  vec_jaccard <- numeric(length = n_overlaps)
+  overlaps_index <- 1
+  
+  for (i in seq_len(n_sets - 1)) {
+    name1 <- set_names[i]
+    set1 <- sets[[i]]
+    for (j in seq(i + 1, n_sets)) {
+      name2 <- set_names[j]
+      set2 <- sets[[j]]
+      
+      set_intersect <- set1[match(set2, set1, 0L)]
+      set_union <- .Internal(unique(c(set1, set2), incomparables = FALSE,
+                                    fromLast = FALSE, nmax = NA))
+      num_shared <- length(set_intersect)
+      overlap <- num_shared / min(length(set1), length(set2))
+      jaccard <- num_shared / length(set_union)
+      
+      vec_name1[overlaps_index] <- name1
+      vec_name2[overlaps_index] <- name2
+      vec_num_sample1[overlaps_index] <- length(set1)
+      vec_num_sample2[overlaps_index] <- length(set2)
+      vec_num_shared[overlaps_index] <- num_shared
+      vec_overlap[overlaps_index] <- overlap
+      vec_jaccard[overlaps_index] <- jaccard
+      
+      overlaps_index <- overlaps_index + 1
+    }
+  }
+  
+  result <- data.frame(sample1 = vec_name1,
+                       sample2 = vec_name2,
+                       num_sample1 = vec_num_sample1,
+                       num_sample2 = vec_num_sample2,
+                       num_shared = vec_num_shared,
+                       overlap = round(vec_overlap, 3),
+                       jaccard = round(vec_jaccard, 3)
+                       )
+  return(result)
+}
+
+
