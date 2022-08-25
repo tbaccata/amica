4dea7d36c3810c39a9ddc4ae6f5882377c075e6c
tbaccata <sebastian.didusch@gmail.com>
2022-08-25 18:44:37 +0200

    reformatted QC plots

--- amica/R/server/qcPlots.R
+++ amica/R/server/qcPlots.R
@@ -1,5 +1,46 @@
 #' Plot density plot
 #' 
+#' @param proteinData ProteomicsData object.
+#' @param assayNames char 
+#' @param groupFactors vector of groups to be ordered.
+#' @return df in long format.
+#' @examples
+getAssayData <-
+  function(proteinData,
+           assayNames = "ImputedIntensity",
+           groupFactors = NULL) {
+    object <- 0
+    if (assayNames == "ImputedIntensity") {
+      object <- assay(proteinData, "ImputedIntensity")
+      
+      object <- toLongFormat(
+        object[isQuantRnames(proteinData), ],
+        proteinData,
+        addGroup = TRUE,
+        addContaminant = FALSE,
+        addGeneName = FALSE
+      )
+    } else {
+      object <- toLongFormat(
+        assay(proteinData, assayNames),
+        proteinData,
+        addGroup = TRUE,
+        addContaminant = FALSE,
+        addGeneName = FALSE
+      )
+    }
+    
+    if (!is.null(groupFactors) &&
+        all(groupFactors %in% unique(object$group))) {
+      object <- object[object$group %in% groupFactors, ]
+      object$group <-
+        factor(object$group, levels = groupFactors)
+    }
+    object
+  }
+
+#' Plot density plot
+#' 
 #' @param dataAssay data.frame in long format.
 #' @param myColors vector of colors.
 #' @param base_size int describing the base plot font size.
@@ -86,6 +127,103 @@ plotBoxPlot <-
     p
   }
 
+#' Compute pca plot
+#' 
+#' @param plotData data.frame of intensities in wide format.
+#' @param myGroupColors vector of colors.
+#' @param boxplot_base int describing the base plot font size.
+#' @param boxplot_legend int describing the legend size.
+#' @param assayNames char 
+#' @param groupFactors vector of groups to be ordered.
+#' @param groupInputs vector of groups to be plotted.
+#' @return list of df_out containing coordinates and percentages char vector of % variance.
+#' @examples
+computePCA <-
+  function(plotData,
+           expDesign,
+           groupFactors = NULL,
+           groupInputs = NULL) {
+    # filter only by selected groups
+    if (is.null(groupInputs))
+      groupInputs <- unique(expDesign$groups)
+    
+    if (!is.null(groupFactors) &&
+        all(groupFactors %in% unique(expDesign$groups))) {
+      expDesign$groups <-
+        factor(expDesign$groups, levels = c(groupFactors,
+                                            setdiff(groupInputs, groupFactors)))
+    }
+    
+    plotData <-
+      plotData[, expDesign$samples[expDesign$groups %in% groupInputs]]
+    plotData <- plotData[complete.cases(plotData),]
+    
+    # compute PCA
+    pca <- prcomp(as.data.frame(t(plotData)))
+    df_out <- as.data.frame(pca$x)
+    
+    df_out$group <-
+      expDesign$groups[expDesign$samples %in% row.names(df_out)]
+    df_out$sample <- rownames(df_out)
+    df_out$key <- df_out$sample
+    df_out$show_id <- FALSE
+    
+    tmp <- summary(pca)
+    percentage <-
+      round(100 * tmp$importance['Proportion of Variance', 1:2], 2)
+    percentage <-
+      paste(colnames(df_out), "(", paste(as.character(percentage), "%", ")", sep =
+                                           ""))
+    return(list(df_out = df_out, percentage = percentage))
+  }
+
+#' Compute pca plot
+#' 
+#' @param df_out df, output from computePCA.
+#' @param percentage char, output from computePCA; vector of %var of 1st and 2nd PC.
+#' @param myGroupColors vector of colors.
+#' @param pca_base int describing the base plot font size.
+#' @param pca_legend int describing the legend size.
+#' @param pca_pointsize int describing ggplot point size.
+#' @param assayNames char 
+#' @param groupFactors vector of groups to be ordered.
+#' @param groupInputs vector of groups to be plotted.
+#' @return list of df_out containing coordinates and percentages char vector of % variance.
+#' @examples
+plotPCA <-
+  function(df_out,
+           percentage,
+           myGroupColors,
+           assayNames = 'LFQIntensity',
+           pca_base = 14,
+           pca_legend = 12,
+           pca_pointsize = 2) {
+    p <-
+      ggplot(df_out, aes(
+        x = PC1,
+        y = PC2,
+        color = group,
+        label = sample,
+        key = key
+      ))
+    p <-
+      p + geom_point(size = pca_pointsize) + xlab(percentage[1]) + ylab(percentage[2])
+    p <-
+      p + scale_color_manual(values = myGroupColors) + theme_cowplot(font_size = pca_base) +
+      background_grid() +
+      theme(legend.text = element_text(size = pca_legend))
+    p <-
+      p + ggtitle(paste0('PCA (', gsub("Intensity", " intensities", assayNames), ')'))
+    
+    p <- p + geom_text(
+      data = subset(df_out, show_id),
+      aes(PC1, PC2, label = sample),
+      position = position_jitter(width = 0.25, height = 0.5)
+    )
+    
+    p
+  }
+
 #' Plot correlation plot (plotly)
 #' 
 #' @param df data.frame with intensity values.
@@ -165,3 +303,47 @@ plotCorrPlotly <-
     p
   }
 
+#' Plot CV boxplot plot (plotly)
+#' 
+#' @param dataAssay df in long format with intensity values.
+#' @param myGroupColors vector of colors.
+#' @param cv_base int describing the base plot font size.
+#' @param cv_legend int describing the legend size.
+#' @param assayNames char 
+#' @return plotly object.
+#' @examples
+plotCoeffVarPlot <-
+  function(dataAssay,
+           myGroupColors,
+           assayNames = 'LFQIntensity',
+           cv_base = 14,
+           cv_legend = 12) {
+    dataAssay$value <- 2 ^ dataAssay$value
+    cvs <-
+      aggregate(value ~ rowname + group, dataAssay, function(x)
+        100 * (sd(x) / mean(x)))
+    
+    p <-
+      ggplot(cvs[!is.na(cvs$value),], aes(x = group, y = value, fill = group)) +
+      geom_boxplot(
+        outlier.shape = NA,
+        outlier.color = NULL,
+        outlier.fill = NULL
+      ) +
+      scale_fill_manual(values = myGroupColors) +
+      theme_cowplot(font_size = cv_base) +
+      background_grid() +
+      theme(
+        axis.text.x = element_text(
+          angle = 90,
+          hjust = 1,
+          vjust = 0.5,
+          size = cv_base
+        ),
+        legend.text = element_text(size = cv_legend),
+        legend.title = element_blank()
+      ) + ylab("Coefficient of Variation (%)") + xlab("") +
+      ggtitle(paste0('CV plot (', gsub("Intensity", " intensities", assayNames), ')'))
+    
+    p
+  }
