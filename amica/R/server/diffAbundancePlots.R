a4f7c61732894d3867a148306fc02b7bd6a411df
tbaccata <sebastian.didusch@gmail.com>
2022-08-28 17:09:36 +0200

    reformatted DA plots

--- amica/R/server/diffAbundancePlots.R
+++ amica/R/server/diffAbundancePlots.R
@@ -1,35 +1,3 @@
-#' prepare MA plot data
-#' 
-#' @param data data.frame, processed output from limma.
-#' @param comparison char describing the group comparison (column pattern in @param data).
-#' @param fcCutoff log2FC threshold.
-#' @return data.frame containing relevant MA plot columns.
-#' @examples
-getMAPlotData <- function(data, comparison, fcCutoff) {
-  check <- FALSE
-  comp <- data[, c(geneName, paste0(logfcPrefix, comparison), paste0(avgExprPrefix, comparison ) )]
-  
-  if(length(grep(padjPrefix, colnames(data) )) > 0 ) {
-    comp$pval <- data[[paste0(padjPrefix, comparison)]]
-    check <- TRUE
-  }
-  colnames(comp) <-
-    gsub(paste0("_", comparison) ,
-         "",
-         colnames(comp))
-  
-  comp$significant <- "no"
-  if (check) {
-    comp$significant[abs(comp$logFC) >= fcCutoff & comp$pval <= 0.05] <-
-      "yes"
-  } else {
-    comp$significant[abs(comp$logFC) >= fcCutoff] <-
-      "yes"
-  }
-  
-  return(comp)
-}
-
 #' MA plot
 #' 
 #' @param pltData data.frame containing relevant columns.
@@ -41,6 +9,36 @@ getMAPlotData <- function(data, comparison, fcCutoff) {
 #' @param pointsize int describing pointsize in scatterplot.
 #' @return ggplot object.
 #' @examples
+#' sourcePath <- "data/PXD0016455/"
+#' 
+#' expDesign <-
+#'   read.table(
+#'     paste0(sourcePath, "design.txt"),
+#'     header = T,
+#'     stringsAsFactors = F
+#'   )
+#' 
+#'   outData <-
+#'     readInAmicaSumm(paste0(sourcePath, "amica_proteinGroups.tsv"),
+#'                     expDesign)
+#'   proteinData <- outData$protData
+#'   contrastMatrix = outData$contrasts
+#'   dataLimma = outData$comparisons
+#'
+#' comparison <- "PGRMC1__vs__MIAPACA" 
+#' pltData <- getVolcanoPlotData(
+#' dataLimma,
+#' sample = comparison,
+#' fcThresh = 1.5,
+#' sigCutoffValue = "adj.p-value"
+#' )
+#' 
+#' xText <- unlist(strsplit(comparison, "__vs__"))
+#' xText <- paste0("log2FC(", xText[1], "/", xText[2], ")")
+#' 
+#' p <- plotMAPlot(pltData, xText)
+#' 
+#' 
 plotMAPlot <- function(pltData,
                        xText,
                        colPalette = NA,
@@ -51,7 +49,6 @@ plotMAPlot <- function(pltData,
   colPalette <- ifelse(is.na(colPalette),
                        brewer.pal(5, "Set2"),
                        colPalette)
-  print(colPalette)
   p <-
     ggplot(pltData,
            aes(
@@ -93,6 +90,31 @@ plotMAPlot <- function(pltData,
 #' @param pvalCutoff numeric, filter on (adj.) p-value.
 #' @return data.frame containing relevant volcano plot columns.
 #' @examples
+#' 
+#' sourcePath <- "data/PXD0016455/"
+#' 
+#' expDesign <-
+#'   read.table(
+#'     paste0(sourcePath, "design.txt"),
+#'     header = T,
+#'     stringsAsFactors = F
+#'   )
+#' 
+#'   outData <-
+#'     readInAmicaSumm(paste0(sourcePath, "amica_proteinGroups.tsv"),
+#'                     expDesign)
+#'   proteinData <- outData$protData
+#'   contrastMatrix = outData$contrasts
+#'   dataLimma = outData$comparisons
+#'
+#' comparison <- "PGRMC1__vs__MIAPACA" 
+#' pltData <- getVolcanoPlotData(
+#' dataLimma,
+#' sample = comparison,
+#' fcThresh = 1.5,
+#' sigCutoffValue = "adj.p-value"
+#' )
+#' 
 getVolcanoPlotData <-
   function(data,
            comparison,
@@ -206,10 +228,40 @@ getVolcanoPlotData <-
 #' @param pointsize int describing pointsize in scatterplot.
 #' @return ggplot object.
 #' @examples
+#' sourcePath <- "data/PXD0016455/"
+#' 
+#' expDesign <-
+#'   read.table(
+#'     paste0(sourcePath, "design.txt"),
+#'     header = T,
+#'     stringsAsFactors = F
+#'   )
+#' 
+#'   outData <-
+#'     readInAmicaSumm(paste0(sourcePath, "amica_proteinGroups.tsv"),
+#'                     expDesign)
+#'   proteinData <- outData$protData
+#'   contrastMatrix = outData$contrasts
+#'   dataLimma = outData$comparisons
+#'
+#' comparison <- "PGRMC1__vs__MIAPACA" 
+#' pltData <- getVolcanoPlotData(
+#' dataLimma,
+#' sample = comparison,
+#' fcThresh = 1.5,
+#' sigCutoffValue = "adj.p-value"
+#' )
+#' 
+#' xText <- unlist(strsplit(comparison, "__vs__"))
+#' xText <- paste0("log2FC(", xText[1], "/", xText[2], ")")
+#' 
+#' p <- plotVolcanoPlot(pltData, xText)
+#' 
+#' 
 plotVolcanoPlot <-
   function(pltData,
            xText,
-           padjYBoolean,
+           padjYBoolean = TRUE,
            colPalette = NA,
            fontsize = 14,
            legend_fontsize = 10,
@@ -299,5 +351,402 @@ getFCPlotData <-
       )
     )
     plotData$key <- plotData[[geneName]]
+    plotData$show_id <- FALSE
     return(plotData)
   }
+
+#' fold-change plot
+#' 
+#' @param plotData data.frame containing relevant columns.
+#' @param selection char vector of length 2, containing group comparison names.
+#' @param colors vector of colors.
+#' @param labels char vector of length 2, containing new labels corresponding to @param selection.
+#' @param showLine one of "straight line", "linear regression" or "none".
+#' @param fontsize int describing the base plot font size.
+#' @param pca_legend int describing the legend size.
+#' @param legendFontSize int describing ggplot point size.
+#' @param pointsize int describing pointsize in scatterplot.
+#' @return ggplot object.
+#' @examples
+plotFoldChangePlot <- function(plotData, 
+                               selection,
+                               colors = NA,
+                               labels = NULL,
+                               showLine = "straight line",
+                               fontsize = 14,
+                               legendFontSize = 12,
+                               pointsize = 2
+                               ) {
+  
+  colors <- ifelse(is.na(colors),
+                       brewer.pal(5, "Set2"),
+                       colors)
+  
+  formula <- as.formula(paste0(paste0("logFC_", selection[2]), ' ~ ',
+                               paste0("logFC_", selection[1])   ) )
+  
+  fit1 <- lm(formula, data=plotData)
+  fit1.intercept <- fit1$coefficients[[1]]
+  fit1.slope <- fit1$coefficients[[2]]
+  
+  eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
+                   list(a = format(unname(coef(fit1)[1]), digits = 2),
+                        b = format(unname(coef(fit1)[2]), digits = 2),
+                        r2 = format(summary(fit1)$r.squared, digits = 3)))
+  txt <- as.character(as.expression(eq))
+  
+  
+  title <-
+    paste0(signif(fit1$coef[[1]], 5), 
+           "x + ", 
+           signif(fit1$coef[[2]], 5),
+           ", r2 = ",
+           signif(summary(fit1)$r.squared, 5))
+  
+  labelNamesX <- paste(unlist(strsplit(selection[1], "__vs__") ), collapse = "/")
+  labelNamesX <- paste0("log2FC(", labelNamesX, ")")
+  labelNamesY <- paste(unlist(strsplit(selection[2], "__vs__") ), collapse = "/")
+  labelNamesY <- paste0("log2FC(", labelNamesY, ")")
+  
+  levels <- c('both', selection[1], selection[2], 'none')
+  if(!is.null(labels) & all(labels != "")) {
+    levels[2] <- labels[1]
+    levels[3] <- labels[2]
+  }
+  
+  plotData$significant <-
+    factor(plotData$significant,
+           levels = levels)
+  
+    pu <-
+      ggplot(plotData,
+             # variable != literal in the R "programming" ""language""
+             aes(
+               x = !!sym(paste0(logfcPrefix, selection[1])),
+               y = !!sym(paste0(logfcPrefix, selection[2])),
+               label = Gene.names,
+               key = key,
+               color = significant
+             )) + geom_point(size = pointsize) + 
+      theme_cowplot(font_size = fontsize) + background_grid() +
+      theme(
+        legend.text = element_text(size = legendFontSize),
+        legend.title = element_text(size = legendFontSize)
+      ) + scale_color_manual(values=colors) +
+      labs(x = labelNamesX, y = labelNamesY) #, title =  title) 
+    
+    intercept <- 0
+    slope <- 1
+    if (showLine == "linear regression") {
+      intercept <- fit1.intercept
+      slope <- fit1.slope
+      pu <- pu + ggtitle(title)
+    } else if (showLine == "straight line") {
+      intercept <- 0
+      slope <- 1
+    }
+    
+    if (showLine != "none") {
+      pu <- pu + geom_abline(
+        intercept = intercept,
+        slope = slope,
+        size = 1,
+        alpha = 0.5,
+        color = colors[5]
+      )
+    }
+    
+    pu <- pu + geom_text(
+      data = subset(plotData, show_id),
+      aes(!!sym(paste0(logfcPrefix, selection[1])),
+          !!sym(paste0(logfcPrefix, selection[2])),
+          label = Gene.names)
+      #hjust=0, vjust=0
+      ,position = position_jitter(width=0.25,height=0.25)
+    )
+}
+
+#' Euler diagram
+#' 
+#' @param comparisons vector of group comparisons.
+#' @param binMat binary data.frame of significant proteins (rows) in comparisons (cols).
+#' @param showQuant boolean, displays number of elements in sets.
+#' @param bool boolean, whether to plot lines around Euler ellipses..
+#' @param showLegend boolean, whether to displahy legend.
+#' @param newMultiNames named list, with old and new names of comparisons
+#' @param colPalette vector, colors.
+#' @return Euler plot.
+#' @examples
+plotEulerDiagram <-
+  function(comparisons,
+           binMat,
+           showQuant = TRUE,
+           bool = TRUE,
+           showLegend = TRUE,
+           newMultiNames = NULL,
+           colPalette = NA,
+           minSizeGradient=1, 
+           maxSizeGradient=7) {
+    if (length(comparisons) < 2) {
+      stop("Need at least two comparisons to render Euler plot.")
+    } else if (length(comparisons) > 5) {
+      stop("Cannot output Euler plot for more than 5 sets.")
+    }
+    
+    colPalette <- ifelse(is.na(colPalette),
+                         brewer.pal(length(comparisons), "Set2"),
+                         colPalette)
+    
+    if (!is.null(newMultiNames) && all(newMultiNames$new != "") &&
+        length(newMultiNames$new) == length(comparisons)) {
+      for (idx in seq_along(newMultiNames$old)) {
+        names(binMat)[which(names(binMat) == newMultiNames$old[idx])] <-
+          newMultiNames$new[idx]
+        comparisons[idx] <- newMultiNames$new[idx]
+      }
+    }
+    
+    fit <- euler(binMat[, comparisons])
+    comps <- grep("&",
+                  names(fit$original),
+                  invert = T,
+                  value = T)
+    numComps <- length(comps)
+    lty <- ifelse(bool, 1, 0)
+    plot(
+      fit,
+      quantities = showQuant,
+      fills = list(fill = colPalette),
+      legend = ifelse(showLegend, list(labels = comps), F),
+      lty = lty
+    )
+  }
+
+#' dotplot data
+#' 
+#' @param widestats data.frame, containing (adj) p-values (rows) in group comparisons (columns).
+#' @param widefcs data.frame, containing log2FCs (rows) in group comparisons (columns).
+#' @param df data.frame, containing intensities (rows) in samples (columns).
+#' @param filtData data.frame, quantified rowData from proteinData.
+#' @param group2comps data.frame, containing group comparisons and corresponding group for dotplot.
+#' @param expDesign named list, with old and new names of comparisons
+#' @param sigCutoffValue char, choice between "adj.P.Val" and "p-value".
+#' @return dotplot plot in long format.
+#' @examples
+getDotplotData <-
+  function(widestats,
+           widefcs,
+           df,
+           filtData,
+           group2comps,
+           expDesign,
+           sigCutoffValue = "p-value") {
+    
+  pattern <- "adj.P.Val_"
+  if (sigCutoffValue == "p-value") pattern <- "P.Value_"
+
+  longStats <- stats::reshape(widestats, idvar = "rowname",
+                              ids = rownames(widestats), times = names(widestats),
+                              timevar = "colname", varying = list(names(widestats)),
+                              direction = "long", v.names = "value")
+  
+  longStats$colname <- gsub(pattern, "", longStats$colname)
+  names(longStats) <- c('Comparison', 'padj', 'ProteinID')
+  
+  longStats <- longStats[longStats$Comparison %in% group2comps$comparison, ]
+  midx <- match(longStats$Comparison, group2comps$comparison)
+  longStats$Group <- group2comps$group[midx]
+  
+  longStats$Comparison <- NULL
+
+  
+  longFCs <- stats::reshape(widefcs, idvar = "rowname",
+                            ids = rownames(widefcs), times = names(widefcs),
+                            timevar = "colname", varying = list(names(widefcs)),
+                            direction = "long", v.names = "value")
+  
+  longFCs$colname <- gsub("logFC_", "", longFCs$colname)
+  names(longFCs) <- c('Comparison', 'log2FC', 'ProteinID')
+  
+  longFCs <- longFCs[longFCs$Comparison %in% group2comps$comparison, ]
+  midx <- match(longFCs$Comparison, group2comps$comparison)
+  longFCs$Group <- group2comps$group[midx]
+  
+  longData <- merge(longFCs, longStats, by = c('ProteinID', 'Group'))
+  
+  intData <- stats::reshape(df, idvar = "rowname",
+                            ids = rownames(df), times = names(df),
+                            timevar = "colname", varying = list(names(df)),
+                            direction = "long", v.names = "value")
+  midx <- match(intData$colname, expDesign$samples)
+  intData$group <- expDesign$groups[midx]
+  
+  intData <- intData[intData$group %in% group2comps$group,]
+  intData <-
+    aggregate(value ~ rowname + group, intData, mean, na.rm=TRUE, na.action=NULL)
+  names(intData) <- c('ProteinID', 'Group', 'AvgIntensity')
+  
+  longData <- merge(longData, intData, by = c('ProteinID', 'Group'))
+  
+  longData$Gene <- filtData[longData$ProteinID, "Gene.names"]
+  longData$Gene <- gsub(";.*", "", longData$Gene)
+
+  longData$significant <- factor(ifelse(longData$padj <= 0.05, 1.5, 0))
+  longData
+}
+
+#' dotplot
+#' 
+#' @param dataDotplot data.frame, output from getDotplotData.
+#' @param dotplotGroupsDf data.frame, containing group comparisons and corresponding group for dotplot.
+#' @param dotplotColors vector of colors.
+#' @param minColorGradient numeric.
+#' @param maxColorGradient numeric.
+#' @param minSizeGradient int.
+#' @param maxSizeGradient int.
+#' @param clusteringMetric char, one of "AvgIntensity" and "log2FC".
+#' @param dotplot_distance_metric char, valid option from hclust.
+#' @param dotplot_clustering_method char, valid option from hclust.
+#' @param dotplot_cluster_columns boolean, performs clustering on columns.
+#' @param sigCutoffValue char, choice between "adj.P.Val" and "p-value".
+#' @param dotplot_ctrl_substraction boolean, ignores proteins with negative log2FCs.
+#' @return ggplot2 object.
+#' @examples
+plotDotplot <-
+  function(dataDotplot,
+           dotplotGroupsDf,
+           dotplotColors=NULL,
+           minColorGradient=NULL,
+           maxColorGradient=NULL,
+           minSizeGradient=1,
+           maxSizeGradient=7,
+           clusteringMetric = "AvgIntensity",
+           dotplot_distance_metric = "canberra",
+           dotplot_clustering_method = "complete",
+           dotplot_cluster_columns=TRUE,
+           sigCutoffValue="p-value",
+           dotplot_ctrl_substraction=TRUE) {
+    
+    if (is.null(dotplotColors))
+      dotplotColors <- viridis(20, option = "viridis")
+    
+  mat <- dataDotplot %>%
+    dplyr::select(Gene, Group, all_of(clusteringMetric)) %>%  # drop unused columns to faciliate widening
+    pivot_wider(names_from = Group, values_from = all_of(clusteringMetric)) %>%
+    data.frame() # make df as tibbles -> matrix annoying
+  
+  row.names(mat) <- mat$Gene  # put gene in `row`
+  mat$Gene <- NULL #drop gene column as now in rows
+  
+  tmat <- mat %>% as.matrix()
+  tmat[is.na(tmat)] <- 0
+  
+  dist_meth <- ifelse(!is.null(dotplot_distance_metric),
+                      dotplot_distance_metric, 
+                      "canberra")
+  clst_meth <- ifelse(!is.null(dotplot_clustering_method),
+                      dotplot_clustering_method,
+                      "complete")
+  
+  if (!is.null(dotplot_cluster_columns) && dotplot_cluster_columns) { # order by clustering
+    cclust <- hclust(dist(t(tmat), method = dist_meth),
+                     method = clst_meth) # hclust with distance matrix
+    cclust <- reorder(as.dendrogram(cclust), colMeans(mat))
+    dataDotplot$Group <-
+      factor(dataDotplot$Group, levels = names(mat)[as.hclust(cclust)$order])
+  } else { # order by input
+    dataDotplot$Group <-
+      factor(dataDotplot$Group, levels = dotplotGroupsDf$group)
+  }
+  
+  rclust <- hclust(dist(tmat, 
+                        method = dist_meth), 
+                   method = clst_meth) # hclust with distance matrix
+  rclust <- reorder(as.dendrogram(rclust), rowMeans(mat))
+  dataDotplot$Gene <-
+    factor(dataDotplot$Gene, levels = rownames(mat)[as.hclust(rclust)$order])
+  
+  
+  signficantTitle <- "adj.p-value"
+  if (sigCutoffValue == "p-value")
+    signficantTitle <- "p-value"
+  
+  filterValues <- TRUE
+  if (clusteringMetric == "log2FC" &&
+      !is.null(dotplot_ctrl_substraction)) {
+    filterValues <- dotplot_ctrl_substraction
+  }
+  
+  preFiltered <- dataDotplot
+  if (filterValues) {
+    preFiltered <- dataDotplot %>% filter(!!rlang::sym(clusteringMetric) > 0)
+  }
+  
+  x <- preFiltered[[clusteringMetric]]
+  
+  legName <- "Relative AvgIntensity"
+  if (clusteringMetric == "log2FC") {
+    preFiltered$relativeAbundance <- (x-min(x,na.rm = T))/(max(x, na.rm = T)-min(x, na.rm = T))
+    legName <- "Relative Fold Change"
+  } else {
+    preFiltered$relativeAbundance <- x/max(x, na.rm = T)
+  }
+  
+  if (is.null(minColorGradient)){
+    minColorGradient < min(preFiltered$log2FC, na.rm = T)
+  }
+  if (is.null(maxColorGradient)){
+    maxColorGradient < max(preFiltered$log2FC, na.rm = T)
+  }
+  
+  p <- preFiltered %>%
+    ggplot(aes(
+      x = Group,
+      y = Gene,
+      fill = log2FC,
+      color = significant,
+      size = relativeAbundance,
+      stroke = 1
+    )) +
+    geom_point(shape = 21) +
+    cowplot::theme_cowplot() +
+    theme(axis.line  = element_blank()) +
+    theme(axis.text.x = element_text(
+      angle = 90,
+      vjust = 0.5,
+      hjust = 1
+    )) +  theme(legend.justification = "top") +
+    ylab('') + xlab('') +
+    scale_x_discrete(position = "top") +
+    scale_size_continuous(
+      range = c(minSizeGradient, maxSizeGradient),
+      name = legName, #paste("Relative", clusteringMetric),
+      guide = guide_legend(order=2, override.aes = list(shape = 19)),
+      breaks = c(
+        min(preFiltered$relativeAbundance ) + 0.01,
+        max(preFiltered$relativeAbundance )
+      ),
+      labels = c('', '')
+    ) +
+    scale_fill_gradientn(
+      colours = dotplotColors,
+      limits = c(
+        ifelse(
+          minColorGradient < min(preFiltered$log2FC, na.rm = T),
+          min(preFiltered$log2FC, na.rm = T),
+          minColorGradient
+        ),
+        maxColorGradient
+      ),
+      oob = scales::squish,
+      name = 'Log2FC',
+      guide = guide_colorbar(order = 1)
+    ) +
+    scale_color_manual(
+      signficantTitle,
+      values = c('skyblue', 'black'),
+      limits = c('0', '1.5'),
+      labels = c('> 0.05', '\u2264 0.05')
+    )
+  p
+}
