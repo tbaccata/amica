8ab6f5dfb583a1fcb477a328270decc18314606f
tbaccata <sebastian.didusch@gmail.com>
2022-08-30 18:22:37 +0200

    added reporting standards modal

--- amica/R/server/modals.R
+++ amica/R/server/modals.R
@@ -567,3 +567,64 @@ output$oraHelpBox <- renderUI({
     )
   }
 })
+
+
+
+
+observeEvent(input$showReportingStandards, {
+  out <- '
+  <h3>Used libraries and ressources</h3>
+         <p>Please cite following publications (if you have used them):</p>
+         <hr>
+         <h4>Upload Analysis options</h4>
+         <ul>
+         <li>(Differential expression analysis) <b>limma:</b> Ritchie, Matthew E., et al. "limma powers
+         differential expression analyses for RNA-sequencing and microarray
+         studies." Nucleic acids research 43.7 (2015): e47-e47.</li>
+         <li>(Differential expression analysis) <b>DEqMS: </b> Zhu, Yafeng, et al. "DEqMS: a method for accurate
+         variance estimation in differential protein expression analysis."
+         Molecular & Cellular Proteomics 19.6 (2020): 1047-1057.</li>
+         </ul>
+         <hr>
+         <h4>Data analysis and visualizations in multiple tabs</h4>
+         <ul>
+         <li>(Heatmaply) <b>heatmaply: </b>Galili, Tal, et al. "heatmaply: an R
+         package for creating interactive cluster heatmaps for online
+         publishing." Bioinformatics 34.9 (2018): 1600-1602.</li>
+         <ul>
+         Following visualizations are created by the <b>heatmaply</b> library:
+         <li>QC tab: interactive correlation plot</li>
+         <li>QC tab: interactive protein overlap plot</li>
+         <li>Differential Abundance tab: interactive heatmap</li>
+         </ul>
+         </ul>
+         <hr>
+         <h4>Data analysis and visualizations in Differential Abundance tab</h4>
+         <ul>
+         <li>(UpSet plot) <b>UpSet: </b> Lex, Alexander, et al. "UpSet:
+         visualization of intersecting sets." IEEE transactions on visualization
+         and computer graphics 20.12 (2014): 1983-1992.</li>
+         The UpSet plot is a powerful visualization for multiple set comparisons.
+         <li>(PPI Networks) <b>IntAct: </b>Orchard, Sandra, et al. "The MIntAct
+         project—IntAct as a common curation platform for 11 molecular
+         interaction databases." Nucleic acids research 42.D1 (2014): D358-D363.</li>
+         <li>(Subcell. localization) <b>Human CellMap: </b>Go, Christopher D., et al.
+         "A proximity-dependent biotinylation map of a human cell." Nature (2021): 1-5.</li>
+         <li>(ORA) <strong>gprofiler2: </strong>Raudvere, Uku, et al. "g: Profiler: a web
+         server for functional enrichment analysis and conversions of gene
+         lists (2019 update)." Nucleic acids research 47.W1 (2019): W191-W198.</li>
+         </ul>
+         </p>
+         <br>
+  '
+  showModal(
+    modalDialog(
+      title = "Reporting standards",
+      HTML(out),
+      size = "l",
+      easyClose = TRUE,
+      footer = NULL
+    )
+  )
+})
+
