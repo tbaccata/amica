fc674c7f0459d0ba65b4b145d4a90d44bc002a88
Juraj Ahel <juraj.ahel@gmail.com>
2024-12-03 17:19:34 +0100

    chore: formatting

--- amica/R/ui/uiAbout.R
+++ amica/R/ui/uiAbout.R
@@ -11,8 +11,7 @@ tabPanel(
          <li>2023-05-30 - New feature added - option to highlight proteins in volcano - and MA plots.</li>
          <li>2023-05-30 - Updated IntAct version (2022-07-13).</li>
          <li>2023-05-30 - Changed default plot colors.</li>
-	 <li>22.12.09 - amica published in <a href="https://doi.org/10.1186/s12864-022-09058-7"
-	 target="_blank">BMC Genomics</a>.</li>
+         <li>2022-12-09 - amica published in <a href="https://doi.org/10.1186/s12864-022-09058-7" target="_blank">BMC Genomics</a>.</li>
          <li>2022-08-30 - New feature added - option to not impute missing values.</li>
          <li>2022-08-30 - New feature added - QC - and diff. abundance reports.</li>
          <li>2022-08-30 - Fixed error handling in upload.</li>
